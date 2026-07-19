82ed0213b564305e35ead063e8813480bc399c94
rishikap-08 <rishikapatidar42@gmail.com>
2026-07-19 15:44:28 +0530

    Add files via upload

---
+++ chapter4.py
@@ -0,0 +1,89 @@
+#output print()
+
+name = "rishika"
+age = 18 
+#print("hi my name is",name,"and my age is",age)
+#print(f"hi my name is {name} and my age is {age}")
+
+#input input()
+#age = int(input("what is your age?"))
+#print("hello your age is ",age)
+
+#arithmetic operations
+#numbers- int,float,complex
+#(+,-,/,//,*,**,%)
+
+a = 10
+b = 3
+#print(a+b+45)
+#print(a-b)
+#print(a/2)
+#print(a//2)
+#print(a*2)
+#print(a**2) #power
+#print(a%b) #modulus reminder jo aata h na division ke baad whai ans h 
+#print(12%5)
+
+"""()-brackets
+**- exponent : right to left: 2**2**3 = 2**(2**3)
+* / // % - multiplication, division, floor division, modulus
++ - addition, subtraction"""
+
+#print(10//2 *5)
+#print(10*2//5)
+#print(3 + 4 * 2)
+#print(15// 4 + 15%4)
+#print(3+2**2*5-1)
+
+#comparison operators
+#(==,!=,>,<,>=,<=)
+#print(10==10)
+#print(10!=10)
+#print(10!=5)
+#print(18>10)
+#print(10<8)
+#print(10>=10)
+#print(10<=50)
+
+a = 10
+b = 20
+#print(b>a)
+
+#and or and not
+#and - both conditions should be true
+#print(a>5 and b>10 and 12==12)
+#print(a<6 and 34==56 and 12==12)
+#print(not 12==56)
+
+'''practice problems'''
+print((5>3 and 10==10) or (4!=4 and 2<1))
+print((10==10 and 23!=23) or(34==12 and bool("hello")) )
+print(not (5==5 and 3!=4) or (10>20))
+
+#assignment operators
+#+= , -= , *= , /= , //= , **= , %=
+a = 10
+a += 20
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
