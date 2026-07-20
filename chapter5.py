64f64f8972c4948364b3668bad321b92075d3dd8
rishikap-08 <rishikapatidar42@gmail.com>
2026-07-20 09:04:02 +0530

    Add files via upload

---
+++ chapter5.py
@@ -0,0 +1,12 @@
+a = 12
+b = 10 
+c = 8
+d = float(a)
+print(b + a //d)
+print(str(b))
+name ='rishika'
+age = 18
+state = ' madhya pradesh'
+city = 'ujjain'
+print("my name is ",name,"my age is ",age,"i live in ",city,"from",state,)
+
